4f8d72438af5b51fd83efe582fd90dabcabd2a7e
monbira <monikabiras@gmail.com>
2023-02-07 19:58:40 +0100

    Rybka plave vpravo 9x.

--- src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
+++ src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
@@ -30,6 +30,16 @@ public class RybkaApplication extends CzechitasGameApplication {
     protected void onStart() {
         kamen = new Kamen(200, 200);
         rybka = new Rybka(100, 100);
+
+        rybka.plavatVpravo();
+        rybka.plavatVpravo();
+        rybka.plavatVpravo();
+        rybka.plavatVpravo();
+        rybka.plavatVpravo();
+        rybka.plavatVpravo();
+        rybka.plavatVpravo();
+        rybka.plavatVpravo();
+        rybka.plavatVpravo();
     }
 
     public static void main(String[] args) {
